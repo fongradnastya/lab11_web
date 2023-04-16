50f217c846179cc8ede5e5275fc11855980eda37
Anastasia Fongrad <fongradnastya@gmail.com>
2023-04-16 14:15:58 +0700

    Added basket control

--- js/basket.js
+++ js/basket.js
@@ -1,34 +1,45 @@
-const basketPrise = $(".price");
-const headerPrice = $(".header__prise");
-const counters = $(".counter");
-
-
 $(".trash").on("click", function (event){
     const card = $(event.target).closest(".card");
     card.remove();
     setPrice();
 });
 
+function checkIsEmpty(){
+    if($(".counter").length == 0){
+        $(".empty").css('display', "block");
+        $(".common-price").css('display', "none");
+        $(".warning").css('display', "block");
+        disableForm();
+    }
+}
+
+function disableForm(){
+    form.check.checked = false;
+    $(".addres").css('display', "none");
+    form.check.disabled = true;
+    form.flexRadio[0].disabled = true;
+    form.flexRadio[1].disabled = true;
+    form.email.disabled = true;
+    form.phone.disabled = true;
+    form.btn.disabled = true;
+}
+
 function setPrice(){
     commonPrice = 0; 
     $(".counter").each(function(i, elem)
     {
         let quantity = elem.value;
-        console.log(quantity);
         const card = $(this).closest(".card");
-        console.log(card);
         const itemPriceTag = card.find(".card-price");
         let itemPrice = Number(itemPriceTag.text().trim().slice(0, -3));
         commonPrice += quantity * itemPrice;
-    }
-    )
-    basketPrise.text(commonPrice + " р.");
-    headerPrice.text(commonPrice + " р.");
-    // checkIsEmpty();
+    })
+    $(".price").text(commonPrice + " р.");
+    $(".header__prise").text(commonPrice + " р.");
+    checkIsEmpty();
 }
 
 $(".counter").each(function(){
-
     $(this).on('input', function() {setPrice()});
 });
 
