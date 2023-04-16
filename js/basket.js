e1dcafb5ce15c2e979ba4c67bce52b127dcbe569
Anastasia Fongrad <fongradnastya@gmail.com>
2023-04-16 13:58:02 +0700

    Added prise calculations

--- js/basket.js
+++ js/basket.js
@@ -1,60 +1,35 @@
-const basketPrise = document.querySelector(".price");
-const headerPrice = document.querySelector(".header__prise");
-const deleteItems = document.querySelectorAll(".trash");
-const counters = document.getElementsByClassName("counter");
+const basketPrise = $(".price");
+const headerPrice = $(".header__prise");
+const counters = $(".counter");
 
-deleteItems.forEach((item)=>{
-    item.addEventListener("click", function (event){
-        const card = event.target.closest(".card");
-        card.parentNode.removeChild(card);
-        setPrice();
-    });
-})
 
-let commonPrice = 0;
-
-function checkIsEmpty(){
-    if(counters.length == 0){
-        const img = document.querySelector(".empty");
-        const commonPrice = document.querySelector(".common-price");
-        const message = document.querySelector(".warning");
-        img.style.display = "block";
-        commonPrice.style.display = "none";
-        message.style.display = "block";
-        disableForm();
-    }
-}
-
-function disableForm(){
-    form.check.checked = false;
-    form.deliveryAdress.style.display = "none";
-    form.check.disabled = true;
-    form.flexRadio[0].disabled = true;
-    form.flexRadio[1].disabled = true;
-    form.email.disabled = true;
-    form.phone.disabled = true;
-    form.btn.disabled = true;
-}
+$(".trash").on("click", function (event){
+    const card = $(event.target).closest(".card");
+    card.remove();
+    setPrice();
+});
 
 function setPrice(){
-    commonPrice = 0;
-    Array.prototype.forEach.call(counters, function(item){
-        let quantity = item.value;
-        const card = item.closest(".card");
-        const itemPriceTag = card.querySelector(".card-price");
-        let itemPrice = Number(itemPriceTag.textContent.trim().slice(0, -3));
+    commonPrice = 0; 
+    $(".counter").each(function(i, elem)
+    {
+        let quantity = elem.value;
+        console.log(quantity);
+        const card = $(this).closest(".card");
+        console.log(card);
+        const itemPriceTag = card.find(".card-price");
+        let itemPrice = Number(itemPriceTag.text().trim().slice(0, -3));
         commonPrice += quantity * itemPrice;
-        
-    });
-    basketPrise.textContent = commonPrice + " р.";
-    headerPrice.textContent = commonPrice + " р.";
-    checkIsEmpty();
+    }
+    )
+    basketPrise.text(commonPrice + " р.");
+    headerPrice.text(commonPrice + " р.");
+    // checkIsEmpty();
 }
 
-Array.prototype.forEach.call(counters, function(item){
-    item.addEventListener("input", ()=>{
-        setPrice();
-    })
+$(".counter").each(function(){
+
+    $(this).on('input', function() {setPrice()});
 });
 
 setPrice();
--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
 // Работа с корзиной
-
-const basketPrise = document.querySelector(".header__prise");
-const productsAdd = document.querySelectorAll(".popular__link");
-basketPrise.textContent = 0 + " р.";
-
+$(".header__prise").text(0 + " р.");
 let commonPrice = 0;
 
-productsAdd.forEach((item)=>{
-    item.addEventListener("click", function (event) {
-        const card = event.target.closest(".popular__card");
-        const productPrice = card.querySelector(".popular__price");
-        let price = Number(productPrice.textContent.trim().slice(0, -3));
-        commonPrice += price;
-        basketPrise.textContent = commonPrice + " р.";
-    })
-})
 
+$(".popular__link").on("click", function (event) {
+    const card = $(event.target).closest('.popular__card');
+    const productPrice = card.find(".popular__price");
+    let price = Number(productPrice.text().trim().slice(0, -3));
+    commonPrice += price;
+    $(".header__prise").text(commonPrice + " р.");
+})
